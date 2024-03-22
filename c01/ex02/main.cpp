5ea8dc986c13c8970a8d1f69621eede184057fea
Maelle <jdoukhan@students.42.fr>
2024-03-22 13:01:05 +0100

    c01/ex02

---
+++ c01/ex02/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdoukhan <jdoukhan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 11:27:28 by jdoukhan          #+#    #+#             */
+/*   Updated: 2024/03/22 13:00:38 by jdoukhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *ptr = &str;
+	std::string &ref = str;
+
+	std::cout << "str adress: " << &str << " = "<< str << std::endl;
+	std::cout << "ptr adress: " << &ptr << " = "<< *ptr << std::endl;
+	std::cout << "ref adress: " << &ref << " = "<< ref << std::endl;
+}
