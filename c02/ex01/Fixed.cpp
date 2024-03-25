772e6f5540f2435dd8ac632bbfe5fc03d46ddec9
Maelle <jdoukhan@students.42.fr>
2024-03-25 14:49:36 +0100

    float working

--- c02/ex01/Fixed.cpp
+++ c02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jdoukhan <jdoukhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 13:05:56 by jdoukhan          #+#    #+#             */
-/*   Updated: 2024/03/25 11:38:05 by jdoukhan         ###   ########.fr       */
+/*   Updated: 2024/03/25 14:49:11 by jdoukhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,45 @@ Fixed& Fixed::operator=( const Fixed& Other )
 
 Fixed::Fixed( const int inb ): _mantissa(inb << this->_expo)
 {
-	
+	std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed( const float fnb )
 {
+	std::cout << "Float constructor called" << std::endl;
 	
+	int dec = (int) (std::fmod(fnb, 1) * 256);
+	this->_mantissa = (int) ((int)( fnb )) << this->_expo | ((int) \
+	((0b10000000 & dec) >> 7) | \
+	((0b01000000 & dec) >> 5) | \
+	((0b00100000 & dec) >> 3) | \
+	((0b00010000 & dec) >> 1) | \
+	((0b00000001 & dec) << 7) | \
+	((0b00000010 & dec) << 5) | \
+	((0b00000100 & dec) << 3) | \
+	((0b00001000 & dec) << 1)\
+	);
 }
+
 
 float	Fixed::toFloat( void ) const
 {
-	
+	float	nb;
+	nb = (float) (this->_mantissa >> this->_expo) + ((float)(\
+	((0b10000000 & this->_mantissa) >> 7) | \
+	((0b01000000 & this->_mantissa) >> 5) | \
+	((0b00100000 & this->_mantissa) >> 3) | \
+	((0b00010000 & this->_mantissa) >> 1) | \
+	((0b00001000 & this->_mantissa) << 1) | \
+	((0b00000100 & this->_mantissa) << 3) | \
+	((0b00000010 & this->_mantissa) << 5) | \
+	((0b00000001 & this->_mantissa) << 7)) / 256);
+	return (nb);
 }
 
 int		Fixed::toInt( void ) const
 {
-	
+	return (roundf((this->toFloat())));
 }
 
 Fixed::~Fixed()
@@ -56,7 +79,7 @@ Fixed::~Fixed()
 
 int		Fixed::getRawBits( void ) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	// std::cout << "getRawBits member function called" << std::endl;
 	return (this->_mantissa);
 }
 
--- c02/ex01/main.cpp
+++ c02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdoukhan <jdoukhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 13:43:35 by jdoukhan          #+#    #+#             */
-/*   Updated: 2024/03/25 11:31:38 by jdoukhan         ###   ########.fr       */
+/*   Updated: 2024/03/25 14:35:43 by jdoukhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 #include <iostream>
 
 int     main( void ) {
-	Fixed       a;
-	Fixed const b( 10 );
+	// Fixed       a;
+	Fixed const b( 1234.4321f );
 	Fixed const c( 42.42f );
-	Fixed const d( b );
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	// Fixed const d( b );
+	// a = Fixed( 1234.4321f );
+	// std::cout << "a is " << a << std::endl;
+	// std::cout << "b is " << b << std::endl;
+	// std::cout << "c is " << c << std::endl;
+	// std::cout << "d is " << d << std::endl;
+	// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as Float" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << c.toFloat() << " as Float" << std::endl;
+	// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
