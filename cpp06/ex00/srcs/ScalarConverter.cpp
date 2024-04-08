92ad65e920c4e49845f892cd0ebedd37ec6071e0
Maelle <jdoukhan@students.42.fr>
2024-04-08 12:09:14 +0200

    finished ex00

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -6,12 +6,13 @@
 /*   By: jdoukhan <jdoukhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 15:37:20 by jdoukhan          #+#    #+#             */
-/*   Updated: 2024/04/08 11:11:25 by jdoukhan         ###   ########.fr       */
+/*   Updated: 2024/04/08 12:09:00 by jdoukhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <cstdlib>
+#include <limits>
 
 void ScalarConverter::convert( const std::string str ) {
 
@@ -21,8 +22,8 @@ void ScalarConverter::convert( const std::string str ) {
 	double	dnb;
 	bool point = 0;
 
-	std::string pliterals[] = { "inf", "inff", "-inf", "-inf" };
-	for (int i = 0; i < 4; i++) {
+	std::string pliterals[] = { "inf", "inff", "-inf", "-inf", "nan" };
+	for (int i = 0; i < 5; i++) {
 		if (str == pliterals[i]) {
 			std::cout	<< "char: impossible value" << std::endl;
 			std::cout	<< "int: impossible value" << std::endl;
@@ -50,46 +51,29 @@ void ScalarConverter::convert( const std::string str ) {
 			std::cout	<< "double: " << std::strtod(str.c_str(), NULL) << std::endl;
 		}
 	}
-	else if (str.end()[-1] == 'f' && (str.size() == (size_t)1 || (str.size() >= (size_t)2 && str.rfind('f', str.size() - 1)))) {
+	else {//if (str.end()[-1] == 'f' && (str.size() == (size_t)1 || (str.size() >= (size_t)2 && str.rfind('f', str.size() - 1)))) {
 		for (size_t i = 0; i < str.size() - 1; i++) {
 			if (!isdigit(str[i]) && ((str[i] == '.' && point) || str[i] != '.')) {
 				std::cout << "Impossible value." << std::endl;
 				return ;
 			}
 			else if ((size_t) i == str.size() - 2) {
-				fnb = std::atof(str.c_str());
+				dnb = std::strtod(str.c_str(), NULL);
+				fnb = static_cast<float>(dnb);
 				if (static_cast<int>(fnb) < 128 && static_cast<int>(fnb) >= -127)
 					std::cout	<< "char: '" << static_cast<char>(fnb) << "'" << std::endl;
 				else
 					std::cout	<< "char: impossible value" << std::endl;
-				std::cout	<< "int: " << static_cast<int>(fnb) << std::endl;
+				if (std::atoll(str.c_str()) > __INT_MAX__ || std::atoll(str.c_str()) < -((long long)__INT_MAX__) - 1)
+					std::cout << "int: impossible value" << std::endl;
+				else
+					std::cout	<< "int: " << std::atoi(str.c_str()) << std::endl;
 				std::cout	<< "float: " << fnb << std::endl;
-				std::cout	<< "double: " << static_cast<double>(fnb) << std::endl;
-				std::cout << "HERE" << std::endl;
+				std::cout	<< "double: " << dnb << std::endl;
 			}
 			if (str[i] == '.')
 				point = true;
 		}
 		return ;
 	}
-	else {
-		for (size_t i = 0; i < str.size(); i++) {
-			if (!isdigit(str[i]) || std::atoll(str.c_str()) > __INT_MAX__ || - std::atoll(str.c_str()) - 1> __INT_MAX__)
-				std::cout << "int: impossible value" << std::endl;
-			else if ((size_t) i == str.size() - 1) {
-				nb = std::atoi(str.c_str());
-				if (nb < 128 && nb >= -128)
-					std::cout	<< "char: '" << static_cast<char>(nb) << "'" << std::endl;
-				else
-					std::cout	<< "char: impossible value" << std::endl;
-				std::cout	<< "int: " << nb << std::endl;
-				std::cout	<< "float: " << static_cast<int>(nb) << std::endl;
-				std::cout	<< "double: " << static_cast<double>(nb) << std::endl;
-
-			}
-			
-		}
-		return ;
-	}
-	(void) dnb;
 }
